8953cfc34843ae16f99319bfebb6fd1e2fb7ebad
Mtnman <billman.designs@gmail.com>
2018-12-02 19:57:17 -0500

    got the data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,45 @@ Brett Billman December 2018
 
 */
 
-var apiKey = pLga1tNmXJRVzNc2V1mHcsMBondNnVSu;
-var queryURL =  
+let topics = ["owl", "elephant", "bear", "shark"];
+
+$(document).ready(function(){
+    
+    //loop through topics array and create button for each
+    for (let i = 0; i < topics.length; i++) {
+        let topicBtn = $("<button>");
+        topicBtn.addClass("api-button");
+        topicBtn.attr("data-animal", topics[i]);
+        topicBtn.text(topics[i]);
+        $("#button-container").append(topicBtn);
+    }
+    
+    $(".api-button").on("click", function() {
+
+        let search = $(this).attr("data-animal");
+
+        console.log(search);
+    
+        let apiKey = "pLga1tNmXJRVzNc2V1mHcsMBondNnVSu";
+        let queryURL = "http://api.giphy.com/v1/gifs/search?q="+search+"&api_key="+apiKey+"&limit=10";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+
+    });
 
 
+});
+
+
+
+
+
+
+});
 
 
 
