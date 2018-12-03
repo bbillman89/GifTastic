d6ed35e5d14d9815e07a8ce6509416cf057043bf
Mtnman <billman.designs@gmail.com>
2018-12-02 22:09:03 -0500

    only need to display the gifs on the page and i'm done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,20 +8,11 @@ Brett Billman December 2018
 let topics = ["owl", "elephant", "bear", "shark"];
 
 $(document).ready(function(){
-    
-    //loop through topics array and create button for each
-    for (let i = 0; i < topics.length; i++) {
-        let topicBtn = $("<button>");
-        topicBtn.addClass("api-button");
-        topicBtn.attr("data-animal", topics[i]);
-        topicBtn.text(topics[i]);
-        $("#button-container").append(topicBtn);
-    }
-    
-    $(".api-button").on("click", function() {
 
+    function animalInfo() {
+    
         let search = $(this).attr("data-animal");
-
+    
         console.log(search);
     
         let apiKey = "pLga1tNmXJRVzNc2V1mHcsMBondNnVSu";
@@ -32,18 +23,42 @@ $(document).ready(function(){
             method: "GET"
         }).then(function(response){
             console.log(response);
+    
+        })
+    }
 
-    });
 
+    
+    //loop through topics array and create button for each
+    function buildBtn () {
+        $("#button-container").empty();
+        for (let i = 0; i < topics.length; i++) {
+            let topicBtn = $("<button>");
+            topicBtn.addClass("api-button");
+            topicBtn.attr("data-animal", topics[i]);
+            topicBtn.text(topics[i]);
+            $("#button-container").append(topicBtn);
+        }
+    }
+
+    
+    $("#add-button").on("click", function() {
+        event.preventDefault();
+        let input = $("#input-value").val().trim();
+        topics.push(input);
+        console.log(input);
+        console.log(topics);
+        $("#input-value").val("");
+        
+        buildBtn();
+    })
+    
+    $(document).on("click", ".api-button", animalInfo)
+    
+    buildBtn();
 
 });
 
-
-
-
-
-
-});
 
 
 
