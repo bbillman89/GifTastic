fcf2a665b0c36e65e9f89090d82d7a6fad69f57a
Mtnman <billman.designs@gmail.com>
2018-12-04 17:11:27 -0500

    coding is complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,8 @@ Brett Billman December 2018
 
 */
 
-let topics = ["owl", "elephant", "bear", "shark"];
+const topics = ["owl", "elephant", "bear", "shark"];
+let ind = 0;
 
 $(document).ready(function(){
 
@@ -23,11 +24,33 @@ $(document).ready(function(){
             method: "GET"
         }).then(function(response){
             console.log(response);
-    
+            for (let ind = 0; ind < response.data.length; ind++) {
+                let gif = $("<img>").attr("src", response.data[ind].images.fixed_width_still.url);
+                gif.attr("data-still", response.data[ind].images.fixed_width_still.url);
+                gif.attr("data-animate", response.data[ind].images.fixed_width.url);
+                gif.attr("data-state", "still");
+                gif.addClass("gif");
+                let rating = response.data[ind].rating;
+                $("#gif-container").prepend(gif);
+                $("#gif-container").prepend("<p> Rating: " + rating + "</p>");
+
+            }
         })
     }
+ 
+    function clickAnimate() {
+        var state = $(this).attr("data-state");
 
+        console.log(state);
 
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    }
     
     //loop through topics array and create button for each
     function buildBtn () {
@@ -53,7 +76,8 @@ $(document).ready(function(){
         buildBtn();
     })
     
-    $(document).on("click", ".api-button", animalInfo)
+    $(document).on("click", ".api-button", animalInfo);
+    $(document).on("click", ".gif", clickAnimate);
     
     buildBtn();
 
